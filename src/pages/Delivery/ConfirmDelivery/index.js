9208b8e4148ce1d06d6155a3776e69b055a8eeb3
Ivens Diego Müller <ivens.muller@amazingit.com.br>
2020-04-19 19:25:53 -0300

    Finishing Mobile App

--- src/pages/Delivery/ConfirmDelivery/index.js
+++ src/pages/Delivery/ConfirmDelivery/index.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
+import {Alert, TouchableOpacity} from 'react-native';
+import {useCamera} from 'react-native-camera-hooks';
+import ImageResizer from 'react-native-image-resizer';
+
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import {
+  PictureButton,
+  SubmitButton,
+  SignaturePreview,
+  Camera,
+  CameraContainer,
+} from './styles';
+
 import {Container, Header} from '~/Components/GlobalStyles/styles';
-import {SubmitButton} from './styles';
 
-export default function ConfirmDelivery({navigation}) {
+import api from '~/services/api';
+
+export default function Confirm({initialProps, navigation}) {
   const delivery = navigation.getParam('delivery');
+  const profile = useSelector((state) => state.user.profile);
 
-  async function handleConfirmDelivery() {}
+  const [file, setFile] = useState(null);
+
+  const [
+    {cameraRef, type, ratio, autoFocus, autoFocusPoint},
+    {takePicture},
+  ] = useCamera(initialProps);
+
+  async function handlePicture() {
+    if (file) {
+      setFile(null);
+      return;
+    }
+
+    const data = await takePicture({quality: 0.5});
+    setFile(data);
+  }
+
+  async function handleSubmit() {
+    try {
+      const resizedImage = await ImageResizer.createResizedImage(
+        file.uri,
+        500,
+        300,
+        'JPEG',
+        100,
+        0,
+        null,
+      );
+
+      const data = new FormData();
+
+      data.append('file', {
+        uri: resizedImage.uri,
+        name: 'file.jpeg',
+        type: 'image/jpeg',
+      });
+
+      const response = await api.post('files', data);
+
+      const {id} = response.data;
+
+      await api.put(`delivery/${delivery.id}/deliver/${profile.id}`, {
+        signature_id: id,
+      });
+
+      Alert.alert('Sucesso', 'Entrega realizada');
+
+      navigation.navigate('Dashboard');
+    } catch (error) {
+      console.tron.log(error);
+      const {response} = error;
+
+      if (response?.data?.error) {
+        Alert.alert('Atenção', response.data.error);
+      } else {
+        Alert.alert(
+          'Atenção',
+          'Não foi possível realizar a entrega, tente novamente mais tarde',
+        );
+      }
+    }
+  }
 
   return (
     <>
       <Header />
       <Container>
-        <SubmitButton onPress={handleConfirmDelivery}>Enviar</SubmitButton>
+        {file ? (
+          <SignaturePreview source={{isStatic: true, uri: file.uri}} />
+        ) : (
+          <CameraContainer>
+            <Camera
+              ref={cameraRef}
+              autoFocusPointOfInterest={autoFocusPoint.normalized}
+              type={type}
+              ratio={ratio}
+              autoFocus={autoFocus}
+              captureAudio={false}
+            />
+          </CameraContainer>
+        )}
+        <PictureButton onPress={handlePicture}>
+          <Icon name="camera" size={36} color="#ffffff" />
+        </PictureButton>
+        <SubmitButton onPress={handleSubmit}>Enviar</SubmitButton>
       </Container>
     </>
   );
 }
 
-ConfirmDelivery.navigationOptions = ({navigation}) => ({
+Confirm.navigationOptions = ({navigation}) => ({
   title: 'Confirmar entrega',
   headerLeft: () => (
     <TouchableOpacity onPress={() => navigation.navigate('DeliveryDetails')}>
--- src/pages/Delivery/ConfirmDelivery/styles.js
+++ src/pages/Delivery/ConfirmDelivery/styles.js
@@ -1,7 +1,49 @@
 import styled from 'styled-components/native';
+import {RNCamera} from 'react-native-camera';
+
 import Button from '~/Components/Button';
 
+export const PictureButton = styled.TouchableOpacity`
+  margin-top: -40px;
+  width: 61px;
+  height: 61px;
+  border-radius: 30.5px;
+  align-self: center;
+  background-color: #0000004d;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const SubmitButton = styled(Button)`
-  margin-top: 5px;
   background: #7d40e7;
+  margin-top: 20px;
+`;
+
+export const TextButton = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+  color: #ffffff;
+`;
+
+export const CameraContainer = styled.View`
+  margin-top: 20px;
+  height: 400px;
+  width: 80%;
+  border-radius: 4px;
+  align-self: center;
+`;
+
+export const Camera = styled(RNCamera)`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  align-self: center;
+  border-radius: 4px;
+`;
+
+export const SignaturePreview = styled.Image`
+  height: 400px;
+  width: 80%;
+  border-radius: 4px;
+  align-self: center;
 `;
